744b7d3fc064dbe6f2242eef0514f408a32a42aa
tethico <tethico@gmail.com>
2020-05-22 20:53:10 +0300

    lab4 re-done

--- lab4/src/MyCar.java
+++ lab4/src/MyCar.java
@@ -9,15 +9,15 @@ import javax.vecmath.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Car implements ActionListener {
+public class MyCar implements ActionListener {
     private float upperEyeLimit = 15.0f;
     private float lowerEyeLimit = 5.0f;
     private float farthestEyeLimit = 28.0f;
     private float nearestEyeLimit = 22.0f;
 
-    private TransformGroup treeTransformGroup;
+    private TransformGroup carTransformGroup;
     private TransformGroup viewingTransformGroup;
-    private Transform3D treeTransform3D = new Transform3D();
+    private Transform3D carTransform3D = new Transform3D();
     private Transform3D viewingTransform = new Transform3D();
     private float angle = 0;
     private float eyeHeight;
@@ -26,10 +26,10 @@ public class Car implements ActionListener {
     private boolean approaching = true;
 
     public static void main(String[] args) {
-        new Car();
+        new MyCar();
     }
 
-    private Car() {
+    private MyCar() {
         Timer timer = new Timer(50, this);
         SimpleUniverse universe = new SimpleUniverse();
 
@@ -43,24 +43,20 @@ public class Car implements ActionListener {
 
     private BranchGroup createSceneGraph() {
         BranchGroup objRoot = new BranchGroup();
+        carTransformGroup = new TransformGroup();
+        carTransformGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
-        treeTransformGroup = new TransformGroup();
-        treeTransformGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         buildCar();
-        objRoot.addChild(treeTransformGroup);
 
-        Background background = new Background(new Color3f(0.9f, 0.9f, 0.9f)); // white color
-        BoundingSphere sphere = new BoundingSphere(new Point3d(0,0,0), 100000);
-        background.setApplicationBounds(sphere);
-        objRoot.addChild(background);
-
-        BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0),10000.0);
-        Color3f light1Color = new Color3f(1.0f, 1, 1);
-        Vector3f light1Direction = new Vector3f(4.0f, -7.0f, -12.0f);
+        objRoot.addChild(carTransformGroup);
+        BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, -0.8, 0.0),100.0);
+        Color3f light1Color = new Color3f(1f, 0.5f, 1f);
+        Vector3f light1Direction = new Vector3f(4.0f, -7.0f, 12.0f);
         DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
         light1.setInfluencingBounds(bounds);
         objRoot.addChild(light1);
 
+        // встановлюємо навколишнє освітлення
         Color3f ambientColor = new Color3f(1.0f, 1.0f, 1.0f);
         AmbientLight ambientLightNode = new AmbientLight(ambientColor);
         ambientLightNode.setInfluencingBounds(bounds);
@@ -98,47 +94,92 @@ public class Car implements ActionListener {
         body2TG.addChild(body2);
         body1TG.addChild(body2TG);
 
-        Box glass = new Box(2, 2.5f, 0.8f, Utils.getGlassAppearence());
+        Box body3 = new Box(2, 3, 1.5f, Utils.getBodyAppearence());
+        TransformGroup body3TG = buildTG(new Vector3f(-4.5f, 0,-1.5f ));
+        body3TG.addChild(body3);
+        body1TG.addChild(body3TG);
+
+        Box glass = new Box(2f, 2.6f, 1.3f, Utils.getGlassAppearence());
         TransformGroup glassTG = buildTG(new Vector3f(2.2f, 0, 1.3f));
         glassTG.addChild(glass);
         body1TG.addChild(glassTG);
 
+        Box glassback = new Box(2f, 2.8f, 1.3f, Utils.getGlassAppearence());
+        TransformGroup glassbackTG = buildTG(new Vector3f(-2.2f, 0, 1.3f));
+        glassbackTG.addChild(glassback);
+        body1TG.addChild(glassbackTG);
+
         Cylinder wheel1 = new Cylinder(1.5f, 0.8f, Utils.getWheelAppearence());
-        TransformGroup wheel1TG = buildTG(new Vector3f(-2.5f, 3, -3));
+        TransformGroup wheel1TG = buildTG(new Vector3f(-3.5f, 3, -3));
         wheel1TG.addChild(wheel1);
         body1TG.addChild(wheel1TG);
 
+        Cylinder wheel1_disk = new Cylinder(1.3f, 0.9f, Utils.getDiskAppearence());
+        TransformGroup wheel1diskTG = buildTG(new Vector3f(-3.5f, 3f, -3f));
+        wheel1diskTG.addChild(wheel1_disk);
+        body1TG.addChild(wheel1diskTG);
+
         Cylinder wheel2 = new Cylinder(1.5f, 0.8f, Utils.getWheelAppearence());
-        TransformGroup wheel2TG = buildTG(new Vector3f(-2.5f, -3, -3));
+        TransformGroup wheel2TG = buildTG(new Vector3f(-3.5f, -3.2f, -3));
         wheel2TG.addChild(wheel2);
         body1TG.addChild(wheel2TG);
+
+        Cylinder wheel2_disk = new Cylinder(1.3f, 0.9f, Utils.getDiskAppearence());
+        TransformGroup wheel2diskTG = buildTG(new Vector3f(-3.5f, -3.2f, -3));
+        wheel2diskTG.addChild(wheel2_disk);
+        body1TG.addChild(wheel2diskTG);
 
         Cylinder wheel3 = new Cylinder(1.5f, 0.8f, Utils.getWheelAppearence());
         TransformGroup wheel3TG = buildTG(new Vector3f(5, 3, -3));
         wheel3TG.addChild(wheel3);
         body1TG.addChild(wheel3TG);
 
+        Cylinder wheel3_disk = new Cylinder(1.3f, 0.9f, Utils.getDiskAppearence());
+        TransformGroup wheel3diskTG = buildTG(new Vector3f(5, 3, -3));
+        wheel3diskTG.addChild(wheel3_disk);
+        body1TG.addChild(wheel3diskTG);
+
         Cylinder wheel4 = new Cylinder(1.5f, 0.8f, Utils.getWheelAppearence());
         TransformGroup wheel4TG = buildTG(new Vector3f(5, -3, -3));
         wheel4TG.addChild(wheel4);
         body1TG.addChild(wheel4TG);
 
-        Box radiator = new Box(2, 2.5f, 0.8f, Utils.getRadiatorAppearence());
-        TransformGroup radiatorTG = buildTG(new Vector3f(6.1f, 0, -1));
-        radiatorTG.addChild(radiator);
-        body1TG.addChild(radiatorTG);
+        Cylinder wheel4_disk = new Cylinder(1.3f, 0.9f, Utils.getDiskAppearence());
+        TransformGroup wheel4diskTG = buildTG(new Vector3f(5, -3, -3));
+        wheel4diskTG.addChild(wheel4_disk);
+        body1TG.addChild(wheel4diskTG);
 
-        Box windows1 = new Box(1, 3.1f, 1, Utils.getGlassAppearence());
-        TransformGroup windows1TG = buildTG(new Vector3f(1.6f, 0, 1.3f));
+        Box radiator1 = new Box(2.1f, 0.3f, 0.9f, Utils.getRadiatorAppearence());
+        TransformGroup radiator1TG = buildTG(new Vector3f(6.1f, -1.6f, -1));
+        radiator1TG.addChild(radiator1);
+        body1TG.addChild(radiator1TG);
+
+        Box radiator2 = new Box(2.1f, 0.3f, 0.9f, Utils.getRadiatorAppearence());
+        TransformGroup radiator2TG = buildTG(new Vector3f(6.1f, -0.5f, -1));
+        radiator2TG.addChild(radiator2);
+        body1TG.addChild(radiator2TG);
+
+        Box radiator3 = new Box(2.1f, 0.3f, 0.9f, Utils.getRadiatorAppearence());
+        TransformGroup radiator3TG = buildTG(new Vector3f(6.1f, 0.5f, -1));
+        radiator3TG.addChild(radiator3);
+        body1TG.addChild(radiator3TG);
+
+        Box radiator4 = new Box(2.1f, 0.3f, 0.9f, Utils.getRadiatorAppearence());
+        TransformGroup radiator4TG = buildTG(new Vector3f(6.1f, 1.6f, -1));
+        radiator4TG.addChild(radiator4);
+        body1TG.addChild(radiator4TG);
+
+        Box windows1 = new Box(1.6f, 3.1f, 1.3f, Utils.getGlassAppearence());
+        TransformGroup windows1TG = buildTG(new Vector3f(1.8f, 0, 1.3f));
         windows1TG.addChild(windows1);
         body1TG.addChild(windows1TG);
 
-        Box windows2 = new Box(1, 3.1f, 1, Utils.getGlassAppearence());
-        TransformGroup windows2TG = buildTG(new Vector3f(-1.6f, 0, 1.3f));
+        Box windows2 = new Box(1.6f, 3.1f, 1.3f, Utils.getGlassAppearence());
+        TransformGroup windows2TG = buildTG(new Vector3f(-1.8f, 0, 1.3f));
         windows2TG.addChild(windows2);
         body1TG.addChild(windows2TG);
 
-        treeTransformGroup.addChild(body1TG);
+        carTransformGroup.addChild(body1TG);
 
     }
 
@@ -148,36 +189,14 @@ public class Car implements ActionListener {
         float delta = 0.03f;
 
         // rotation of the castle
-        treeTransform3D.rotZ(angle);
-        treeTransformGroup.setTransform(treeTransform3D);
+        carTransform3D.rotZ(angle);
+        carTransformGroup.setTransform(carTransform3D);
         angle += delta;
 
-        // change of the camera position up and down within defined limits
-        if (eyeHeight > upperEyeLimit){
-            descend = true;
-        }else if(eyeHeight < lowerEyeLimit){
-            descend = false;
-        }
-        if (descend){
-            eyeHeight -= delta;
-        }else{
-            eyeHeight += delta;
-        }
 
-        // change camera distance to the scene
-        if (eyeDistance > farthestEyeLimit){
-            approaching = true;
-        }else if(eyeDistance < nearestEyeLimit){
-            approaching = false;
-        }
-        if (approaching){
-            eyeDistance -= delta;
-        }else{
-            eyeDistance += delta;
-        }
 
-        Point3d eye = new Point3d(eyeDistance, eyeDistance, eyeHeight); // spectator's eye
-        Point3d center = new Point3d(.0f, .0f ,0.1f); // sight target
+        Point3d eye = new Point3d(20.0f, 1.0f, 6.0f); // spectator's eye
+        Point3d center = new Point3d(0f, .0f ,1.0f); // sight target
         Vector3d up = new Vector3d(.0f, .0f, 1.0f);; // the camera frustum
         viewingTransform.lookAt(eye, center, up);
         viewingTransform.invert();
--- lab4/src/Utils.java
+++ lab4/src/Utils.java
@@ -6,23 +6,35 @@ public class Utils {
     public static Appearance getBodyAppearence() {
         Appearance ap = new Appearance();
 
-        Color3f emissive = new Color3f(new Color(0,0, 0));
-        Color3f ambient = new Color3f(Color.RED);
-        Color3f diffuse = new Color3f(Color.RED);
-        Color3f specular = new Color3f(new Color(0,0, 0));
+        Color3f emissive = new Color3f(new Color(30,50,30));
+        Color3f ambient = new Color3f(Color.GREEN);
+        Color3f diffuse = new Color3f(Color.GREEN);
+        Color3f specular = new Color3f(new Color(30,50,30));
         // ambient, emissive, diffuse, specular, 1.0f
         ap.setMaterial(new Material(ambient, emissive, diffuse, specular, 1.0f));
 
         return ap;
     }
 
+    public static Appearance getDiskAppearence() {
+        Appearance ap = new Appearance();
+
+        Color3f emissive = new Color3f(new Color(0,0, 0));
+        Color3f ambient = new Color3f(Color.GRAY);
+        Color3f diffuse = new Color3f(new Color(220,220, 255));
+        Color3f specular = new Color3f(new Color(220,220, 255));
+        // ambient, emissive, diffuse, specular, 1.0f
+        ap.setMaterial(new Material(ambient, emissive, diffuse, specular, 1.0f));
+
+        return ap;
+    }
 
     public static Appearance getRadiatorAppearence() {
         Appearance ap = new Appearance();
 
         Color3f emissive = new Color3f(new Color(0,0, 0));
         Color3f ambient = new Color3f(Color.BLACK);
-        Color3f diffuse = new Color3f(Color.GRAY);
+        Color3f diffuse = new Color3f(new Color(10,70,10));
         Color3f specular = new Color3f(Color.GRAY);
         // ambient, emissive, diffuse, specular, 1.0f
         ap.setMaterial(new Material(ambient, emissive, diffuse, specular, 1.0f));
@@ -46,10 +58,10 @@ public class Utils {
     public static Appearance getGlassAppearence() {
         Appearance ap = new Appearance();
 
-        Color3f emissive = new Color3f(new Color(0,0, 0));
+        Color3f emissive = new Color3f(new Color(0,60, 0));
         Color3f ambient = new Color3f(Color.BLACK);
         Color3f diffuse = new Color3f(Color.LIGHT_GRAY);
-        Color3f specular = new Color3f(Color.LIGHT_GRAY);
+        Color3f specular = new Color3f(new Color(200,250,200));
         // ambient, emissive, diffuse, specular, 1.0f
         ap.setMaterial(new Material(ambient, emissive, diffuse, specular, 1.0f));
 
